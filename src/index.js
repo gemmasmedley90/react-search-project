60c7ce5d46b861b3baa2276f8db0632497b3e333
gemmasmedley90 <gemmasmedley90@gmail.com>
2025-02-18 16:45:17 +0000

    Search project in react

--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import "./index.css";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import Search from "./Search";
+import "./App.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <StrictMode>
+    <div className="App">
+      <div>
+        <h1>Weather App</h1>
+        <Search />
+      </div>
+    </div>
+    <footer>
+      <p>
+        This project was coded by Gemma Smedley and is
+        <a href="https://github.com/gemmasmedley90/" target="_blank">
+          {" "}
+          on GitHub
+        </a>{" "}
+        and
+        <a href="#" target="_blank">
+          hosted on Netlify
+        </a>
+      </p>
+    </footer>
+  </StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
